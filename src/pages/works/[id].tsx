743508526ec3e89fd425936c69d32202b80d2526
tatsuro <oscarwild13@gmail.com>
2021-06-08 14:16:10 +0900

    works詳細コンポーネント

---
+++ src/pages/works/[id].tsx
@@ -0,0 +1,56 @@
+import { ThemeProvider } from '@material-ui/styles';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import PageTemplate from '../../components/layouts/PageTemplate';
+import theme from '../../components/utils/theme';
+import Post from '../../components/works/Post';
+import { getAllPosts, getPostById } from '../../lib/api';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getAllPosts();
+  const paths = posts.map(({ id }) => `/works/${id}`);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id: string = String(params.id);
+  const post = await getPostById(id);
+
+  return {
+    props: { post },
+    revalidate: 3,
+  };
+};
+
+interface Post {
+  post: {
+    id: string;
+    title: string;
+    thumbnail: {
+      url: string;
+    };
+    body: HTMLElement;
+  };
+}
+
+const worksId = ({ post }: Post) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <PageTemplate
+        title={`${post.title} | Works | Sixth Project 13's Profile`}
+      >
+        <Post
+          id={post.id}
+          title={post.title}
+          thumbnail={post.thumbnail.url}
+          body={post.body}
+        />
+      </PageTemplate>
+    </ThemeProvider>
+  );
+};
+
+export default worksId;
